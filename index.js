8ed20ca32c4573a20cb136c58814523446edbd76
Rajkumar <angothurajkumar2002@gmail.com>
2022-08-21 16:07:51 +0530

    mongodb connect

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 //change
-const CONNECTION_URL = 'mongodb+srv://user123:user123@cluster0.4rogdmo.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://user:user123@cluster0.vpfw0pq.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
